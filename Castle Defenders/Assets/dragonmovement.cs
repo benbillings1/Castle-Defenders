082b9ec811f41afb29fa4c2c8b1f54d939ac351a
benbilli <benbilli@iu.edu>
2019-04-09 17:37:06 -0400

    Made dragon move from start node to end node

---
+++ Castle Defenders/Assets/dragonmovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragonmovement : MonoBehaviour
+{
+    public Transform endNode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = new Vector3(endNode.position.x, transform.position.y, endNode.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * 10f);
+    }
+}
